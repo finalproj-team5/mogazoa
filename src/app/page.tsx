01226c30d318d04facdb3801f74782607c3ab11f
Sea0Soda <gotjd9704@gmail.com>
2025-08-29 02:43:03 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24'>
       인덱스 페이지
-      <Button variant='outline' className='ml-4'>
+      <Button variant='primary' className='ml-4'>
         Outline
       </Button>
     </main>
