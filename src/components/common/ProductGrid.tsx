f8bb210b705d9cb8e497ec5a73550ba53104e380
jin <999gin@naver.com>
2025-08-29 17:58:21 +0900

    [상진규] feat : pc,태블릿 카테고리메뉴 제작

--- src/components/common/ProductGrid.tsx
+++ src/components/common/ProductGrid.tsx
@@ -4,7 +4,7 @@ import ProductCard from '@/components/common/ProductCard';
 export default function ProductGrid({ title, products }: ProductGridProps) {
   return (
     <>
-      <div>{title} </div>
+      <h1 className='text-[#F1F1F5] lg:text-2xl text-xl font-semibold'>{title} </h1>
       <div className='grid grid-cols-2 lg:grid-cols-3 gap-[15px] lg:gap-5 justify-center'>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
